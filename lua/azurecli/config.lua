e44573c1a178a3dbbd797507150fa464ce046632
Dan Nicholls <dannicholls12@gmail.com>
2024-03-08 16:58:11 +1100

    feat(config): Add base config
    + Added base config for storing any global settings or configs for the
      user

---
+++ lua/azurecli/config.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+M.types = {
+	icons = {
+		["Task"] = "󰅌",
+		["User Story"] = "",
+		["Bug"] = "",
+		["Defect"] = "",
+		["Default"] = "+",
+	},
+
+	colors = {
+		["Task"] = "#a4880a", -- Example task color
+		["User Story"] = "#0098c7", -- Example user story color
+		["Bug"] = "#c4283b",
+		["Defect"] = "#f06673",
+	},
+}
+
+return M
