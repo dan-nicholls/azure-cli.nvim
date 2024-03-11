102ed65f1865a6b0f9ab59ac59381735f1708fb2
Dan Nicholls <dannicholls12@gmail.com>
2024-03-11 20:38:37 +1100

    fix(api): Update api calls to sort by ID
    As the API are limited to returning a certain number of items, we have
    prioritised and sorted by the latest ID work items.

--- lua/azurecli/api.lua
+++ lua/azurecli/api.lua
@@ -9,42 +9,42 @@ end
 
 function Api.get_all_items()
 	local command =
-		"az boards query --wiql \"SELECT [System.Id], [System.Title], [System.State], [System.WorkItemType] FROM WorkItems WHERE [System.WorkItemType] IN ('Task', 'User Story', 'Bug', 'Defect')\""
+		"az boards query --wiql \"SELECT [System.Id], [System.Title], [System.State], [System.WorkItemType] FROM WorkItems WHERE [System.WorkItemType] IN ('Task', 'User Story', 'Bug', 'Defect') ORDER BY [System.ID] DESC\""
 	result = execute_command(command)
 	return result
 end
 
 function Api.get_tasks()
 	local command =
-		"az boards query --wiql \"SELECT [System.Id], [System.Title], [System.State], [System.WorkItemType] FROM WorkItems WHERE [System.WorkItemType] = 'Task'\""
+		"az boards query --wiql \"SELECT [System.Id], [System.Title], [System.State], [System.WorkItemType] FROM WorkItems WHERE [System.WorkItemType] = 'Task' ORDER BY [System.ID] DESC\""
 	result = execute_command(command)
 	return result
 end
 
 function Api.get_user_stories()
 	local command =
-		"az boards query --wiql \"SELECT [System.Id], [System.Title], [System.State], [System.WorkItemType] FROM WorkItems WHERE [System.WorkItemType] = 'User Story'\""
+		"az boards query --wiql \"SELECT [System.Id], [System.Title], [System.State], [System.WorkItemType] FROM WorkItems WHERE [System.WorkItemType] = 'User Story' ORDER BY [System.ID] DESC\""
 	result = execute_command(command)
 	return result
 end
 
 function Api.get_bugs()
 	local command =
-		"az boards query --wiql \"SELECT [System.Id], [System.Title], [System.State], [System.WorkItemType] FROM WorkItems WHERE [System.WorkItemType] = 'Bug'\""
+		"az boards query --wiql \"SELECT [System.Id], [System.Title], [System.State], [System.WorkItemType] FROM WorkItems WHERE [System.WorkItemType] = 'Bug' ORDER BY [System.ID] DESC\""
 	result = execute_command(command)
 	return result
 end
 
 function Api.get_defects()
 	local command =
-		"az boards query --wiql \"SELECT [System.Id], [System.Title], [System.State], [System.WorkItemType] FROM WorkItems WHERE [System.WorkItemType] = 'Defect'\""
+		"az boards query --wiql \"SELECT [System.Id], [System.Title], [System.State], [System.WorkItemType] FROM WorkItems WHERE [System.WorkItemType] = 'Defect' ORDER BY [System.ID] DESC\""
 	result = execute_command(command)
 	return result
 end
 
 function Api.get_issues()
 	local command =
-		"az boards query --wiql \"SELECT [System.Id], [System.Title], [System.State], [System.WorkItemType] FROM WorkItems WHERE [System.WorkItemType] = 'Issue'\""
+		"az boards query --wiql \"SELECT [System.Id], [System.Title], [System.State], [System.WorkItemType] FROM WorkItems WHERE [System.WorkItemType] = 'Issue' ORDER BY [System.ID] DESC\""
 	result = execute_command(command)
 	return result
 end
